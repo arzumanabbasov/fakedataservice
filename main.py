55a0aa1cc2194992455d4063cebfc2fb2af5ca37
Arzuman Abbasov <106863886+arzumanabbasov@users.noreply.github.com>
2023-04-25 11:41:54 +0400

    Update main.py

--- main.py
+++ main.py
@@ -48,8 +48,7 @@ def main():
     st.write(df.head())
     if st.button("Download"):
         progress_bar = st.progress(0)
-        for i in range(100):
-            time.sleep(3)
+        for i in range(1000):
             progress_bar.progress(i+1)
         progress_bar.empty()
         csv = df.to_csv(index=False)
