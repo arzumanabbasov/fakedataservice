68f6c753d5896a19f6c69dc6bd364de08f315c53
Arzuman Abbasov <106863886+arzumanabbasov@users.noreply.github.com>
2023-04-25 20:25:20 +0400

    Update main.py

--- main.py
+++ main.py
@@ -47,12 +47,6 @@ def main():
     df = pd.DataFrame(data)
     st.write(df.head())
     if st.button("Download"):
-        """
-        progress_bar = st.progress(0)
-        for i in range(1000):
-            progress_bar.progress(i+1)
-        progress_bar.empty()
-        """
         csv = df.to_csv(index=False)
         b64 = base64.b64encode(csv.encode()).decode()
         href = f'<a href="data:file/csv;base64,{b64}" download="data.csv">Download CSV File</a>'
